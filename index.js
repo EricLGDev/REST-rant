1ec9582b752f91d4fc20a7bfd06447c358202bfe
Eric <ericlg.link@gmail.com>
2022-09-13 02:42:04 -0700

    Added 404 page route

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page Not Found</h1>')
+})
+
 app.listen(process.env.PORT)
