d239c8d5f94f2dddc84149876e509fc12b282e9d
Eric <ericlg.link@gmail.com>
2022-09-18 09:47:23 -0700

    Added navbar with css styling

--- views/default.jsx
+++ views/default.jsx
@@ -8,7 +8,21 @@ function Def (html) {
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossOrigin="anonymous"/>
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
-            <body>{html.children}</body>
+            <body>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
+                {html.children}</body>
         </html>
     )
 }
