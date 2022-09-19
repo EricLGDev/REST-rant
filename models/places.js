d9041d3bbd5a4127a4d2866e6ad88159090d5ede
Eric <ericlg.link@gmail.com>
2022-09-18 18:59:03 -0700

    Edit functionality works

--- models/places.js
+++ models/places.js
@@ -3,11 +3,13 @@ module.exports = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: '/images/patrick-tomasso-rest.jpg'
+    pic: '/images/patrick-tomasso-rest.jpg',
+    id: 0
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: '/images/paul-griffin-rest.jpg'
+    pic: '/images/paul-griffin-rest.jpg',
+    id: 1
 }]
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,18 +5,26 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
-                <h2>Rating</h2>
-                <h2>Comments</h2>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                    Edit
-                </a>  
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>     
-
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>Located in {data.place.city}, {data.place.state} </h3>
+                    </div>
+                    <div className="col-sm-6"> 
+                        <h1>{ data.place.name }</h1>
+                        <h2>Serving {data.place.cuisines} </h2>
+                        <h2>Rating</h2>
+                        <h2>Comments</h2>
+                        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                            Edit
+                        </a>  
+                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                            <button type="submit" className="btn btn-danger">
+                                Delete
+                            </button>
+                        </form>  
+                    </div>  
+                </div>
             </main>
         </Def>
     )
