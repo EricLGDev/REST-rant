cca11deccfe1f08e21684359ebb06bb8be64c469
Eric <ericlg.link@gmail.com>
2022-09-27 17:17:30 -0700

    comment section added to show page, need to adjust routing to post comments

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -24,9 +24,13 @@ function edit_form (data) {
                             <label htmlFor="state">State</label>
                             <input className="form-control" id="state" name="state" defaultValue={data.place.state}/>
                         </div>
-                        <div className="form-group">
+                        <div className="form-group col-sm-4">
                             <label htmlFor="cuisines">Cuisines</label>
                             <input className="form-control" id="cuisines" name="cuisines" defaultValue={data.place.cuisines} required />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="founded">Founded</label>
+                            <input className="form-control" id="founded" name="founded" defaultValue={data.place.founded}/>
                         </div>
                         <input className="btn btn-primary" type="submit" value="Add Place" />
                     </div>
