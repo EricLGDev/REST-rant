95c567a2a656b42949e00a34b6fee07943160a51
Eric <ericlg.link@gmail.com>
2022-09-27 17:48:02 -0700

    sample comment works

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -44,45 +44,43 @@ function show (data) {
     }
     return (
         <Def>
-            <main>
-                <div className="row">
-                    <div className="col-sm-6">
-                        <img src={data.place.pic} alt={data.place.name} />
-                        <h3>
-                            Located in {data.place.city}, {data.place.state} 
-                        </h3>
-                    </div>
-                    <div className="col-sm-6"> 
-                        <h1>
-                            { data.place.name }
-                        </h1>
-                        <h2>
-                            Serving {data.place.cuisines} 
-                        </h2>
-                        <h2>
-                            Rating
-                        </h2>
-                        <h2>
-                            {rating}
-                        </h2>
-                        <h3>
-                            {data.place.showEstablished()}
-                        </h3>
-                        <h4>
-                            Serving {data.place.cuisines}
-                        </h4>
-                        <h2>Comments</h2>
-                        <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                            Edit
-                        </a>  
-                        <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                            <button type="submit" className="btn btn-danger">
-                                Delete
-                            </button>
-                        </form>  
-                    </div>  
-                </div>
-                <h2>Comments</h2>
+          <main>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                {rating}
+                <br />
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  {data.place.showEstablished()}
+                </h3>
+                <h4>
+                  Serving {data.place.cuisines}
+                </h4>
+                <br />
+                <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">
+                  Edit
+                </a>{` `}
+                <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}>
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </div>
+            <hr />
+            <h2>Comments</h2>
             <div className="row">
               {comments}
             </div>
@@ -111,7 +109,7 @@ function show (data) {
               </div>
               <input type="submit" className="btn btn-primary" value="Add Comment" />
             </form>
-            </main>
+          </main>
         </Def>
     )
 }
